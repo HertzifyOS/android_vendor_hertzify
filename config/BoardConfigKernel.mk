LineageOS/android_vendor_lineage

kernel: Add RBE support
build/make hands compiles to reclient's rewrapper when USE_RBE is set,
but the kernel build never did. build/make/core/rbe.mk is only included
while dumping the product config, so its RBE_* variables can't be reused
here and the flags have to be recreated.

Only actual compilations can go to rewrapper: reclient's clang input
processor doesn't understand the other things kbuild runs the compiler
for, and it doesn't derive the -Wp,-MD dependency file as an output, so
a cache hit would restore the object without it and fixdep would fail.
A small wrapper handles both and becomes the kernel's CC, replacing
ccache, which can't cache anything behind another wrapper anyway.

Tested on guacamole: a from scratch kernel build drops from 7:56 to 4:03
with a warm cache, with 3904 of the 3909 objects bit identical to a
build with RBE disabled.

Assisted-by: ClaudeCode:claude-opus-5
Change-Id: Iad462ab0ce3d1dadab0f1540d4ff7039e67eb0d8

config/BoardConfigKernel.mk
@@ -45,6 +45,7 @@
 #                                          building. Defaults to empty
 #
 #   USE_CCACHE                         = Enable ccache (global Android flag)
+#   USE_RBE                            = Enable RBE (global Android flag)
 
 include vendor/lineage/build/core/utils.mk
 
@@ -93,6 +94,30 @@ ifneq ($(USE_CCACHE),)
         # Android 10+ deprecates use of a build ccache. Only system installed ones are now allowed
         CCACHE_BIN := $(CCACHE_EXEC)
     endif
+endif
+
+# build/make/core/rbe.mk is only read while dumping the product config, so the
+# rewrapper flags have to be recreated here
+KERNEL_RBE_WRAPPER :=
+ifneq ($(filter-out false,$(USE_REWRAPPER)),)
+    # An out dir outside of the tree can't be a remote input or output
+    ifneq ($(filter $(BUILD_TOP)/%,$(abspath $(OUT_DIR))),)
+        KERNEL_RBE_WRAPPER := $(abspath $(if $(RBE_DIR),$(RBE_DIR),prebuilts/remoteexecution-client/live))/rewrapper
+        KERNEL_RBE_WRAPPER += --labels=type=compile,lang=cpp,compiler=clang
+        KERNEL_RBE_WRAPPER += --env_var_allowlist=PWD
+        KERNEL_RBE_WRAPPER += --exec_strategy=$(if $(RBE_CXX_EXEC_STRATEGY),$(RBE_CXX_EXEC_STRATEGY),local)
+        KERNEL_RBE_WRAPPER += --compare=$(if $(RBE_CXX_COMPARE),$(RBE_CXX_COMPARE),false)
+        ifneq ($(RBE_platform),)
+            KERNEL_RBE_WRAPPER += --platform=$(RBE_platform),Pool=$(if $(RBE_CXX_POOL),$(RBE_CXX_POOL),default)
+        endif
+    endif
+endif
+
+# ccache can't cache anything behind another wrapper, so it gives way to RBE
+ifneq ($(KERNEL_RBE_WRAPPER),)
+    KERNEL_CC_WRAPPER := $(BUILD_TOP)/vendor/lineage/build/tools/kernel_rbe_cc.sh
+else
+    KERNEL_CC_WRAPPER := $(CCACHE_BIN)
 endif
 
 # Clear this first to prevent accidental poisoning from env

HertzifyOS/android_vendor_hertzify

Signed-off-by: HDzungx <hdzungx@gmail.com>

config/BoardConfigKernel.mk
@@ -45,6 +45,7 @@
 #                                          building. Defaults to empty
 #
 #   USE_CCACHE                         = Enable ccache (global Android flag)
+#   USE_RBE                            = Enable RBE (global Android flag)
 
 include vendor/hertzify/build/core/utils.mk
 
@@ -93,6 +94,30 @@ ifneq ($(USE_CCACHE),)
         # Android 10+ deprecates use of a build ccache. Only system installed ones are now allowed
         CCACHE_BIN := $(CCACHE_EXEC)
     endif
+endif
+
+# build/make/core/rbe.mk is only read while dumping the product config, so the
+# rewrapper flags have to be recreated here
+KERNEL_RBE_WRAPPER :=
+ifneq ($(filter-out false,$(USE_REWRAPPER)),)
+    # An out dir outside of the tree can't be a remote input or output
+    ifneq ($(filter $(BUILD_TOP)/%,$(abspath $(OUT_DIR))),)
+        KERNEL_RBE_WRAPPER := $(abspath $(if $(RBE_DIR),$(RBE_DIR),prebuilts/remoteexecution-client/live))/rewrapper
+        KERNEL_RBE_WRAPPER += --labels=type=compile,lang=cpp,compiler=clang
+        KERNEL_RBE_WRAPPER += --env_var_allowlist=PWD
+        KERNEL_RBE_WRAPPER += --exec_strategy=$(if $(RBE_CXX_EXEC_STRATEGY),$(RBE_CXX_EXEC_STRATEGY),local)
+        KERNEL_RBE_WRAPPER += --compare=$(if $(RBE_CXX_COMPARE),$(RBE_CXX_COMPARE),false)
+        ifneq ($(RBE_platform),)
+            KERNEL_RBE_WRAPPER += --platform=$(RBE_platform),Pool=$(if $(RBE_CXX_POOL),$(RBE_CXX_POOL),default)
+        endif
+    endif
+endif
+
+# ccache can't cache anything behind another wrapper, so it gives way to RBE
+ifneq ($(KERNEL_RBE_WRAPPER),)
+    KERNEL_CC_WRAPPER := $(BUILD_TOP)/vendor/hertzify/build/tools/kernel_rbe_cc.sh
+else
+    KERNEL_CC_WRAPPER := $(CCACHE_BIN)
 endif
 
 # Clear this first to prevent accidental poisoning from env